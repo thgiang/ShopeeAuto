68166ba1159cde97cb8f7becd7f30c7e84e3df0d
Admin <admin@gmail.com>
2020-02-14 05:47:40 +0700

    Xong login Shopee

--- ShopeeAuto/ShopeeAuto/ShopeeWorker.cs
+++ ShopeeAuto/ShopeeAuto/ShopeeWorker.cs
@@ -17,38 +17,61 @@ namespace ShopeeAuto
             bool needToLogin = false;
             Global.AddLog("Kiểm tra Shopee đã đăng nhập chưa");
             Global.driver.Navigate().GoToUrl("https://banhang.shopee.vn/account/signin");
+
+            // Chờ tối đa 5s xem có thấy form login hay không.
             try
             {
-                if (Global.wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"app\"]/div[2]/div/div[1]/div/div/div[3]/div/div/div/form/button/span"))).Text == "Đăng nhập")
-                {
-                    Global.AddLog("Chưa đăng nhập, đang đăng nhập");
-                    needToLogin = true;
-                }
-            }
-            catch (OpenQA.Selenium.WebDriverTimeoutException)
+                Global.wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("form.signin-form")));
+                Global.AddLog("Chưa đăng nhập, đang đăng nhập");
+                needToLogin = true;
+            } catch
             {
                 Global.AddLog("Đã đăng nhập");
+                return true;
             }
 
+            // Nếu có form login thì lấy thông tin username và pass từ server
             if(needToLogin)
             {
                 Dictionary<string, string> parameters = new Dictionary<string, string>();
                 dynamic results = new ExpandoObject();
 
-                // Get username and password from API server
+                // Lấy thông tin username và pass từ server
                 parameters["route"] = "client/info";               
                 results = Global.api.Request(parameters);
+
+                // Lấy được user và pass, tiến hành login vào shopee
                 if(results.status == "success")
                 {
-                    Console.WriteLine(results.data.username);
+                    IWebElement loginForm = Global.driver.FindElement(By.CssSelector("form.signin-form"));
+                    loginForm.FindElements(By.TagName("input"))[0].SendKeys(results.data.username.ToString()); // Username
+                    loginForm.FindElements(By.TagName("input"))[1].SendKeys(results.data.password.ToString()); // Password
+                    loginForm.FindElement(By.ClassName("shopee-checkbox__indicator")).Click(); // Remember me
+                    loginForm.FindElement(By.ClassName("shopee-button--primary")).Click(); // Login now
+
+                    // Check lại xem có lỗi gì khi đăng nhập ko, nếu có thì hiển thị, nếu ko thì login thành công
+                    try
+                    {
+                        if(Global.wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("login-error"))).Text != "")
+                        {
+                            string loginError = loginForm.FindElement(By.ClassName("login-error")).Text;
+                            Global.AddLog("Đăng nhập lỗi: " + loginError);
+                            return false;
+                        }
+                    } catch
+                    {
+                        Global.AddLog("Đăng nhập thành công");
+                    }
                     return true;
-                } else
+                }
+                // Lỗi khi gọi lên server lấy username, pass
+                else
                 {
-                    Global.AddLog("Lỗi lấy username, pass từ server. Nội dung: " + results.message);
+                    Global.AddLog("Lỗi lấy username, pass từ server. Nôi: " + results.message);
                     return false;
                 }
             }
-            
+
             return true;
         }
     }
